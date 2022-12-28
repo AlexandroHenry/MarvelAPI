623995dbd3cd8ecf29391070f9d372d44e5bd259
alaxhenry0121 <alaxhenry0121@gmail.com>
2022-12-28 13:33:48 +0900

    Initial Commit

---
+++ MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by Seungchul Ha on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
